8445650a034ff628928345bd16f4c3af169c095b
Mark Gray <mark,gray@microsoft.com>
2021-01-05 13:56:44 -0800

    fixed syntax errors

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "425_demo_app" {
+resource "azurerm_resource_group" "demo_app" {
   name              = "425-demo-app"
-  location          = "westu2"
+  location          = "westus2"
 }
